dcbda7457ee34d20080d7c8c40ed7cafe2f47a17
Darren2017 <17362990052@163.com>
2018-05-17 16:21:04 +0800

    emm...偷个懒，和lab11一样

---
+++ lab12.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<math.h>
+
+int pre(int x);
+void swap(int a, int b);
+
+int main()
+{
+    int a, b, cnt = 0;
+    printf("please input two numbers: ");
+    scanf("%d %d", &a, &b);
+    if(a > b)   swap(a, b);
+    for(int i = a; i <= b; i++){
+        if(pre(i)){
+            printf("%-4d   ", i);
+            cnt++;
+            if(cnt % 9 == 0){
+                printf("\n");
+            }
+        }
+    }
+    printf("\nthe number of prime number is %d", cnt);
+}
+
+void swap(int a, int b){
+    int temp = a;
+    a = b;
+    b = temp; 
+}
+
+int pre(int x)
+{
+    if(x == 1)  return 0;
+    int k = 2;
+    while(k <= (int)sqrt(x)){
+        if(x % k == 0){
+            return 0;
+        }
+        k++;
+    }
+    return 1;
+}
